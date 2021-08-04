78ef9e459e9d48094deb5917e33f1b9ab3500a3b
Eduard Lind <lind.deu@gmail.com>
2021-08-04 21:20:20 +0200

    Basic version without styling ready

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let gameIdString;
   let gameId;
   
+  // add new game to list
   const addGameButton = document.getElementById('addGameAlert');
   addGameButton.addEventListener('click', function() { 
       var highestPrice = document.getElementById('lowestPriceInput').value;
@@ -18,51 +19,24 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }, false);
 
+  // show list of games
   const savedGamesList = document.getElementById('listOfSavedGames');
-  savedGamesList.addEventListener('click', function() {
-      var table = document.getElementById('savedGamesTable');
+  savedGamesList.addEventListener('toggle', function() {
+    // show games
+    if (savedGamesList.open) {
       chrome.storage.sync.get(["GamesList"], async (result) => {
         var savedTable = result["GamesList"];
         if(savedTable) {
-          for(let string=0; string < getAmountOfSavedGames(savedTable); string++) {
-            const oneGameObject = JSON.parse(Object.values(savedTable)[string]);
-            var gameID = Object.values(oneGameObject)[0];
-            tr = table.insertRow(-1);
-            for(let column=0; column < 5; column++) {
-              switch(column) {
-                case 3:
-                {
-                  var td = document.createElement('td');
-                  td = tr.insertCell(-1);
-                  await fetch(`https://www.allkeyshop.com/blog/wp-admin/admin-ajax.php?action=get_offers&product=${gameID}&currency=eur&region=&moreq=&use_beta_offers_display=1`)
-                    .then(response => response.json())
-                    .then(data => {
-                      const currentPrice = data.offers[0].price.eur.price;
-                      td.innerHTML = currentPrice;
-                    });
-                  break;
-                }
-                case 4:
-                {
-                  var td = document.createElement('td');
-                  td = tr.insertCell(-1);
-                  td.innerHTML = '<button>Delete from list</button>';
-                  break; 
-                }
-                default:
-                {
-                  var td = document.createElement('td');
-                  td = tr.insertCell(-1);
-                  td.innerHTML = Object.values(oneGameObject)[column];
-                  break; 
-                }
-              } 
-            }
-          }
+          await showSavedTable(savedTable);
         }
       });
+    }
+    if(!savedGamesList.open) {
+      document.getElementById("savedGamesTableBody").innerHTML ="";
+    }
   }, false);
-  
+
+  // clear list of games
   const clearButton = document.getElementById('clearList');
   clearButton.addEventListener('click', function() { 
     chrome.storage.sync.set({ "GamesList": [] }, function(){
@@ -70,10 +44,34 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, false);
 
-}, false);
+  // delete single game from list
+  document.querySelector('#savedGamesTableBody').addEventListener('click', async event => {
+      var target = event.target;
+      if (target.tagName.toLowerCase() === 'button') {
+        var cellOfButton = event.target.parentNode;
+        var position = cellOfButton.parentNode.rowIndex
+        deleteGameFromTableOnPosition(position);
+      }
+    })
+
+  }, false);
+
+
+function deleteGameFromTableOnPosition(position){
+    chrome.storage.sync.get(["GamesList"], async (result) => {
+      var savedTable = result["GamesList"];
+      savedTable.splice(position - 1 , 1);
+      chrome.storage.sync.set({ "GamesList": savedTable }, function(){
+        console.log('table saved');
+    });
+    document.getElementById("savedGamesTableBody").innerHTML ="";
+    await showSavedTable(savedTable);
+  });
+}
+
 
 function setGameName(gameName){
-  document.getElementById('gameNameToAdd').innerHTML = gameName[0].result;
+  document.getElementById('gameNameToAdd').innerHTML = `Add "${gameName[0].result}" to list with max price:`;
 }
 
 async function saveGame(gameId, gameName, highestPrice) {
@@ -106,4 +104,44 @@ function getGameName() {
 
 function getAmountOfSavedGames( savedGames ) {
   return Object.keys(savedGames).length;
+}
+
+async function showSavedTable(savedTable) {
+  var table = document.getElementById('savedGamesTableBody');
+  for(let string=0; string < getAmountOfSavedGames(savedTable); string++) {
+    const oneGameObject = JSON.parse(Object.values(savedTable)[string]);
+    var gameID = Object.values(oneGameObject)[0];
+    tr = table.insertRow(-1);
+    tr.setAttribute('id', 'gameRow');
+    for(let column=0; column < 5; column++) {
+      switch(column) {
+        case 3:
+        {
+          var td = document.createElement('td');
+          td = tr.insertCell(-1);
+          await fetch(`https://www.allkeyshop.com/blog/wp-admin/admin-ajax.php?action=get_offers&product=${gameID}&currency=eur&region=&moreq=&use_beta_offers_display=1`)
+            .then(response => response.json())
+            .then(data => {
+              const currentPrice = data.offers[0].price.eur.price;
+              td.innerHTML = currentPrice;
+            });
+          break;
+        }
+        case 4:
+        {
+          var td = document.createElement('td');
+          td = tr.insertCell(-1);
+          td.innerHTML = '<button class="small red button">Delete</button>';
+          break; 
+        }
+        default:
+        {
+          var td = document.createElement('td');
+          td = tr.insertCell(-1);
+          td.innerHTML = Object.values(oneGameObject)[column];
+          break; 
+        }
+      } 
+    }
+  }
 }
